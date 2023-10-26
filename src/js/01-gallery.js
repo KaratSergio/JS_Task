da92fd285833bd8a195a90d9bf09bc09b13118e6
Sergio <137483700+KaratSergio@users.noreply.github.com>
2023-10-26 15:19:14 +0300

    add task 1
    gallery by (SimpleLightbox)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"; 
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+    const galleryItem = document.createElement("li");
+    galleryItem.classList.add("gallery__item");
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.href = item.original;
+
+    const image = document.createElement("img");
+    image.classList.add("gallery__image");
+    image.src = item.preview;
+    image.alt = item.description;
+
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    galleryContainer.appendChild(galleryItem);
+});
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+    captions: true,
+    captionsData: "alt",
+    captionsDelay: 250
+});
 
 console.log(galleryItems);
