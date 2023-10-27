0ef74fe713bbef91256f2d357f0ef7d8fb0d4ffe
Sergio <137483700+KaratSergio@users.noreply.github.com>
2023-10-27 16:54:13 +0300

    add tasks
    1. Image gallery
    2. Video playback

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import VimeoPlayer from '@vimeo/player/dist/player.es.js';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+
+const player = new VimeoPlayer(iframe);
+
+const saveTimeToLocalStorage = throttle(time => {
+    localStorage.setItem('videoplayer-current-time', time);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+    const currentTime = data.seconds;
+    saveTimeToLocalStorage(currentTime);
+});
+
+window.addEventListener('load', () => {
+    const storedTime = localStorage.getItem('videoplayer-current-time');
+
+    if (storedTime) {
+    player.setCurrentTime(parseFloat(storedTime));
+    }
+});
